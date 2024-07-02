ab8a68a958e7bbd10aad0cfb9e4997541d2d3a23
Kwaay <Kwaay@users.noreply.github.com>
2024-07-02 09:08:34 +0000

    deploy: 1e3e5312caa240f5f693b671ae0992967859c8f3

--- types/components/Modal/Modal.d.ts
+++
@@ -1,4 +0,0 @@
-import { ReactElement } from 'react';
-import { ModalProps } from './Modal.types';
-declare function Modal({ type, title, content, width, height, position, delay, }: ModalProps): ReactElement;
-export default Modal;
