d60301d1ee41ba9fd7e168d5c000199f65dc6c9c
Kwaay <Kwaay@users.noreply.github.com>
2024-07-02 22:29:51 +0000

    deploy: fe7fedd658a8d31e2efb4efb84b8878ddae64ee5

---
+++ assets/Modal.stories-DlNZuUoa.js
@@ -0,0 +1,89 @@
+import{R as v}from"./index-uubelm5h.js";import{M as c}from"./Modal-UZrq6y2P.js";const B={component:c,render:q=>v.createElement(c,{...q}),title:"Modal"},t={args:{type:"success",title:"Success",content:"Success",height:"150px",width:"300px",position:"center",delay:5e3,buttonPosition:"upright"}},n={args:{type:"error",title:"Error",content:"Error",height:"150px",width:"300px",position:"center",delay:5e3,buttonPosition:"upright"}},e={args:{type:"info",title:"Info",content:"Info",height:"150px",width:"300px",position:"center",delay:5e3,buttonPosition:"upright"}},o={args:{type:"warning",title:"Warning",content:"Warning",height:"150px",width:"300px",position:"center",delay:5e3,buttonPosition:"upright"}},r={args:{type:"loading",title:"Loading",content:"Loading",height:"150px",width:"300px",position:"center",delay:5e3,buttonPosition:"upright"}},i={args:{type:"launch",title:"Launch",content:"Launch",height:"150px",width:"300px",position:"center",delay:5e3,buttonPosition:"upright"}},s={args:{type:"wait",title:"Wait",content:"Wait",height:"150px",width:"300px",position:"center",delay:5e3,buttonPosition:"upright"}},a={args:{type:"end",title:"End",content:"End",height:"150px",width:"300px",position:"center",delay:5e3,buttonPosition:"upright"}};var p,d,u;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    type: 'success',
+    title: 'Success',
+    content: 'Success',
+    height: '150px',
+    width: '300px',
+    position: 'center',
+    delay: 5000,
+    buttonPosition: 'upright'
+  }
+}`,...(u=(d=t.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var g,h,l;n.parameters={...n.parameters,docs:{...(g=n.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    type: 'error',
+    title: 'Error',
+    content: 'Error',
+    height: '150px',
+    width: '300px',
+    position: 'center',
+    delay: 5000,
+    buttonPosition: 'upright'
+  }
+}`,...(l=(h=n.parameters)==null?void 0:h.docs)==null?void 0:l.source}}};var m,x,y;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    type: 'info',
+    title: 'Info',
+    content: 'Info',
+    height: '150px',
+    width: '300px',
+    position: 'center',
+    delay: 5000,
+    buttonPosition: 'upright'
+  }
+}`,...(y=(x=e.parameters)==null?void 0:x.docs)==null?void 0:y.source}}};var w,b,P;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    type: 'warning',
+    title: 'Warning',
+    content: 'Warning',
+    height: '150px',
+    width: '300px',
+    position: 'center',
+    delay: 5000,
+    buttonPosition: 'upright'
+  }
+}`,...(P=(b=o.parameters)==null?void 0:b.docs)==null?void 0:P.source}}};var E,S,L;r.parameters={...r.parameters,docs:{...(E=r.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    type: 'loading',
+    title: 'Loading',
+    content: 'Loading',
+    height: '150px',
+    width: '300px',
+    position: 'center',
+    delay: 5000,
+    buttonPosition: 'upright'
+  }
+}`,...(L=(S=r.parameters)==null?void 0:S.docs)==null?void 0:L.source}}};var W,f,I;i.parameters={...i.parameters,docs:{...(W=i.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    type: 'launch',
+    title: 'Launch',
+    content: 'Launch',
+    height: '150px',
+    width: '300px',
+    position: 'center',
+    delay: 5000,
+    buttonPosition: 'upright'
+  }
+}`,...(I=(f=i.parameters)==null?void 0:f.docs)==null?void 0:I.source}}};var M,R,_;s.parameters={...s.parameters,docs:{...(M=s.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    type: 'wait',
+    title: 'Wait',
+    content: 'Wait',
+    height: '150px',
+    width: '300px',
+    position: 'center',
+    delay: 5000,
+    buttonPosition: 'upright'
+  }
+}`,...(_=(R=s.parameters)==null?void 0:R.docs)==null?void 0:_.source}}};var O,j,k;a.parameters={...a.parameters,docs:{...(O=a.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    type: 'end',
+    title: 'End',
+    content: 'End',
+    height: '150px',
+    width: '300px',
+    position: 'center',
+    delay: 5000,
+    buttonPosition: 'upright'
+  }
+}`,...(k=(j=a.parameters)==null?void 0:j.docs)==null?void 0:k.source}}};const C=["Success","Error","Info","Warning","Loading","Launch","Wait","End"];export{a as End,n as Error,e as Info,i as Launch,r as Loading,t as Success,s as Wait,o as Warning,C as __namedExportsOrder,B as default};
--- assets/preview-BgJQm_Pp.js
+++
@@ -1,1 +0,0 @@
-import"./Modal-CS-1VPiO.js";import"./index-uubelm5h.js";const t={parameters:{controls:{expanded:!0}}};export{t as default};
